7a3e048c827f95eb3b0637730b447d9ac8a32b4f
Lucas Rezende Mota <70916102+mota-rezende@users.noreply.github.com>
2023-06-12 18:52:16 -0300

    Delete q2.py

--- lista_1/q2.py
+++
@@ -1,7 +0,0 @@
-# Escrever um programa que receba do teclado um preço e retorne uma parcela de doze do valor indicado, com duas casas decimais de arredondamento
-
-preco = float(input())
-
-parcela = preco/12
-
-print("%.2f" % (parcela))
