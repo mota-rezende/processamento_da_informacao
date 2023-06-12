8ea6834ca36b3e1f9ae01262c23229fbea3d5adc
Lucas Rezende Mota <70916102+mota-rezende@users.noreply.github.com>
2023-06-12 18:52:05 -0300

    Delete q1.py

--- lista_1/q1.py
+++
@@ -1,6 +0,0 @@
-# Escreva um programa que leia do teclado dois números e mostre a soma destes numeros com duas casas decimais
-
-a = float(input())
-b = float(input())
-
-print("Resultado = %.2f" % (a+b))
